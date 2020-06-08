8d4d59cf60db72b57d46fd43596a48e5a2c0fdf8
rtestard <rtestard@opensense.fr>
2020-06-08 13:54:35 +0200

    simple get request

---
+++ index.py
@@ -0,0 +1,18 @@
+import requests
+from pprint import pprint
+
+url = 'https://api.kanye.rest/'
+
+
+def fetchData(url):
+    try:
+        r = requests.get(url)
+        json_data = r.json()
+        pprint(json_data['quote'])
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        raise SystemExit(e)
+
+fetchData(url)
+
+
